023f32681194fc95b53c60f04ce33d2b186f538b
Andrew Metzgus <andrew@metzg.us>
2022-07-03 14:17:12 -0700

    Add validators.

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -13,6 +13,10 @@ const Form = ({passFormData}) => {
     const [address, setAddress] = useState('');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
+    const [phoneInvalid, setPhoneInvalid] = useState(false);
+    const [emailInvalid, setEmailInvalid] = useState(false);
+    const [phoneErrorMessage, setPhoneErrorMessage] = useState('');
+    const [emailErrorMessage, setEmailErrorMessage] = useState('');
 
     const handleClear = (e) => {
         e.preventDefault();
@@ -21,6 +25,11 @@ const Form = ({passFormData}) => {
         setAddress("");
         setPhone("");
         setEmail("");
+        setPhoneErrorMessage("");
+        setEmailErrorMessage("");
+        setPhoneInvalid(false);
+        setEmailInvalid(false);
+        
     };
 
     const handleSubmit = (e) => {
@@ -31,12 +40,38 @@ const Form = ({passFormData}) => {
             phone: phone,
             email: email
         });
-    }
+    };
+
+    const phoneChange = (e) => {
+        setPhoneInvalid(false);
+        setPhoneErrorMessage("");
+
+        const re = /\(?[2-9]\d{2}\)?[-. ]?[2-9]\d{2}[-.]?\d{4}$/;
+        if (!e.target.value.match(re)) {
+            setPhoneInvalid(true);
+            setPhoneErrorMessage("Please enter a valid phone number.")
+        }
+        setPhone(e.target.value);
+
+    };
+
+    const emailChange = (e) => {
+        setEmailInvalid(false);
+        setEmailErrorMessage("");
+
+        const re = /^\w+[_.]?\w+@\w+-?\w+\.\w{2,10}$/g;
+        if (!e.target.value.toLowerCase().match(re)) {
+            setEmailInvalid(true);
+            setEmailErrorMessage("Please enter a valid email address.");
+        }
+        setEmail(e.target.value);
+
+    };
 
   return (
     <form data-testid="form" onSubmit={handleSubmit}>
         <Stack spacing={2}>
-            <Typography sx={{textAlign: 'center'}}variant="h5">AvailJS Signup Form</Typography>
+            <Typography variant="h5">AvailJS Signup Form</Typography>
             <TextField 
                 label="Full Name" 
                 variant='filled' 
@@ -63,20 +98,27 @@ const Form = ({passFormData}) => {
                 variant='filled' 
                 required
                 value={phone}
-                onChange={e => setPhone(e.target.value)}
+                onChange={phoneChange}
+                error={phoneInvalid ? true : false}
+                helperText={phoneErrorMessage}
                 />
             <TextField 
                 label="Email" 
                 variant='filled' 
                 required
                 value={email}
-                onChange={e => setEmail(e.target.value)}
+                onChange={emailChange}
+                error={emailInvalid ? true : false}
+                helperText={emailErrorMessage}
                 />
             <div>
                 <Button variant="contained" onClick={handleClear}>
                     Clear
                 </Button>
-                <Button type="submit" variant="contained" color="secondary">
+                <Button type="submit" 
+                    variant="contained" 
+                    color="secondary" 
+                    disabled={phoneInvalid || emailInvalid ? true : false}>
                     Submit
                 </Button>
             </div>
